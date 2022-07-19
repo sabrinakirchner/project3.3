0e7df61f822d7fcc259fd15536c1b57f7f2805fa
Sabrina Kirchner <sabrinapskirchner@outlook.com>
2022-07-19 10:21:37 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+score = float(input("Enter Score: "))
+
+if score >= 0.9 and score <= 0.99:
+    print("A")
+elif score >= 0.8 and score <= 0.9:
+    print("B")
+elif score >= 0.7 and score <= 0.8:
+    print("C")
+elif score >= 0.6 and score <= 0.7:
+    print("D")
+elif score >= 0.5 and score <= 0.6:
+    print("F")
+
+else:
+    print("Error") 
